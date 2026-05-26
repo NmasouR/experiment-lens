b8ef2f4bcc5bbdef584fd7190b299d02debcdb24
Nikos Masouras <63796040+NmasouR@users.noreply.github.com>
2026-05-26 11:36:06 +0300

    re reun deploy

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 const MOBILE_BREAKPOINT = 768;
 
+//hook to use mobile
+
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
