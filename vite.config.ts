b79f249d1483030fe3ddf6bcd6746c6623072a04
Nikos Masouras <63796040+NmasouR@users.noreply.github.com>
2026-05-26 11:49:06 +0300

    base url change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/experiment-lens/" : "/",
+  base: "/experiment-lens/",
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
     dedupe: [
